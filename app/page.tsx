28744808aa68e59360e8d9143e3ff456b0464fbe
Raunak Das <dasraunak5@gmail.com>
2024-04-28 12:20:24 +0530

    Changes to Button componenet

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ButtonGradient from '../components/ButtonGradient'
+import ButtonGradient from '@/components/ButtonGradient'
+import Button from '@/components/Button'
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+       <Button className="mt-10" href="#login">
+          something
+       </Button>
+      </div>
       <ButtonGradient />
     </>
   )
